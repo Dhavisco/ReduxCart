61a5ac9497f9fb20a36346dd77eded588548dc28
Dhavisco <olaniyidavid2002@gmail.com>
2024-03-18 11:33:34 +0100

    displays addedItem quantity

--- src/store/cart-slice.jsx
+++ src/store/cart-slice.jsx
@@ -11,7 +11,8 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find(item => item.id === newItem.id)
-    if(!existingItem){
+        state.totalQuantity++
+      if(!existingItem){
         state.items.push({
             itemId: newItem.id,
             price: newItem.price,
@@ -27,6 +28,7 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
         const id = action.payload;
         const existingItem = state.items.find(item => item.id === id);
+        state.totalQuantity--;
         if(existingItem.quantity === 1){
             state.items = state.items.filter(item => item.id !== id);
     }else{

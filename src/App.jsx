005758cdad6615e07d733949e2a9b4296b2795eb
Dhavisco <olaniyidavid2002@gmail.com>
2024-03-22 09:00:13 +0100

    state changed in App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,11 @@ useEffect(() => {
       isInitial = false;
       return;
     }
-dispatch(sendCartData(cart));
+
+    if(cart.changed){
+      dispatch(sendCartData(cart));
+    }
+
   
   }, [cart, dispatch]);
 

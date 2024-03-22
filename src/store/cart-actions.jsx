bb5ec2a701dc4d678ac4930a3fc021cc60e9040a
Dhavisco <olaniyidavid2002@gmail.com>
2024-03-22 09:05:56 +0100

    remove staus changed from firebase db

--- src/store/cart-actions.jsx
+++ src/store/cart-actions.jsx
@@ -46,7 +46,10 @@ export const sendCartData = (cart) => {
         "https://reduxcart-89c68-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
